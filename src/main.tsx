330a06730fec15ef581d2178a04943b87bdd7761
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-07-28 02:37:59 +0900

    fix: queryClient defaultOption 설정

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,15 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./contexts/AuthProvider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
