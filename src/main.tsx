bc7619604c2cb6905ac6fc81f193c9cc0b2af938
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-07-27 21:01:32 +0900

    fix: queryClient 따로 선언

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,12 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./contexts/AuthProvider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={new QueryClient()}>
+      <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
           <AuthProvider>
             <App />
