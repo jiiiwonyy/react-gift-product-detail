550f4a6b91af0ba5b116fc2dc941c84baa92c515
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-08-01 17:15:25 +0900

    chore: 패키지 관련 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
